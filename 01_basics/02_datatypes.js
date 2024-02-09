da1ee28966593241fdd6ce7f7c2a0310e33f004c
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-09 08:15:42 +0000

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all js code as nwe version of js
+
+console.log(3
++3)// not the best practice, reduce the read abiity
+
+let name = "Ayan" //string
+let age = 18 //number
+let isLoggedIn = false; //boolean
+let state = null
+let temp;
+
+/*data types(premitive)
+number => range 2 power 53
+bigint +> takes bigger values
+string => ""
+boolean => true or false
+null => standalone values(means empty)
+undefined => value is not defined
+symbol => find the uniquenes
+*/
+
+//object 
+console.log(typeof state)// object
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof temp) //undefined
+
