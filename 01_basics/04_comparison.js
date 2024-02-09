dfbf7935f258d5fd8f11d6575ed2e214191c1d71
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-09 19:41:09 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+/* console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+
+console.log(undefined>0)
+console.log(undefined==0) // == equality check
+console.log(undefined>=0)// >= <= > < comparison */
+
+// ************** avoid the above type of comparison
+
+
+//=== strict check (it check the datatype)
+
+console.log("2" == 2) // string == number true 
+console.log("2" === 2) // string === number false because it check the data tye
+
+
