3fd626a8dbc16e67ab727732c66070ec1ab62338
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-09 07:49:15 +0000

    varibales in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const studentId = 123
+let studentName = "Ayan Maity"
+var studentRoll = 156;
+enrollmentNo = 345;
+let className;
+
+/* var is not used bcause of scope problem . Instead of var let is used*/
+console.log(studentId)
+console.table([studentName,studentRoll,enrollmentNo,className])//for printing multiple values in tabular form
