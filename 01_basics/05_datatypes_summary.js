2196a5fe3a03c4b1136cc5364ffecaa2914bc46e
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-09 21:43:51 +0000

    memory stack and heap

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -5,7 +5,7 @@
 // reference(non premitive)
 //Arrya , function , object 
 
-const id = Symbol('123')
+ /* const id = Symbol('123')
 const anotherId = Symbol('123') // symobl used to define the uniqueness
 console.log(id === anotherId)
 
@@ -15,7 +15,26 @@ const heros = [ 'shaktiman' , 'naagraj' , 'doga']
     age : 21,  // { object declared in curly braces }
  }
 
-const myFunction /* function name */= function(){
+ const myFunction = function(){
     console.log("Hello World")
+} 
+// return of function will be function 
+*/
+//++++++++++++++++++++++++++++++ Memory
+//stack(premitive) , heap(non premitive)
+
+/*  let myName = "Ayan"
+let anotherName = myName
+anotherName = "Ayan Maity" 
+console.log(myName)
+console.log(anotherName)  */
+
+let myDetailsOne = {
+    name: "Ayan",
+    email : "Ayan@gmail.com"
 }
-// return of function will be function
+
+let myDetailsTwo = myDetailsOne;
+myDetailsTwo.email = "ayanmaity@gmail.com"
+console.log(myDetailsOne.email)
+console.log(myDetailsTwo.email)
