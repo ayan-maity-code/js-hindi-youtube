d97e191729096c5978487116f00b4a51e005fede
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-09 11:21:56 +0000

    conversion of operator

---
+++ 01_basics/03_conversionoperators.js
@@ -0,0 +1,56 @@
+/* let score = "33abc"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let score1 = null
+console.log(typeof score1)
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+let score2=undefined;
+console.log(typeof score2)
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2) */
+
+/*
+conversion
+33 => 33
+33abc => NaN
+null => 0
+undefined => NaN
+true/false => 1/0
+"ayan" => NaN
+*/
+
+
+/* let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+let isLoggedIn1 = ""
+let booleanisLoggedIn1 = Boolean(isLoggedIn1)
+console.log(booleanisLoggedIn1)
+
+let isLoggedIn2 = "ayan"
+let booleanisLoggedIn2 = Boolean(isLoggedIn2)
+console.log(booleanisLoggedIn2) */
+
+/*
+conversion boolean
+1 => true
+0 => false
+"ayan" => true
+"" => false
+*/
+
+let number = 33
+let stringNumber = String(number) // s will be capital
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
