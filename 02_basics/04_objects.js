81ffea89da1b85cf0cdae233201beef2c385fed0
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-14 21:08:29 +0000

    object part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,52 @@
+// object with constructor
+// this is called singleton 
+const tinderUser = Object() // constructor(singleton object)
+// const tinderUser = {} // non singleton object 
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.age = 18
+
+// console.log(tinderUser)
+
+//object in object
+const newUser = {
+    email: "ayanmaity@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname : "ayan",
+            lastname : "maity"
+        }
+    }
+}
+
+// access object in object 
+
+/* console.log(newUser.fullname)
+console.log(newUser.fullname.userfullname)
+console.log(newUser.fullname.userfullname.firstname)
+console.log(newUser.fullname.userfullname.lastname) */
+// merge two object
+const obj1 = { 1:"a" , 2:"b"}
+const obj2 = { 3:"c" , 4:"d"}
+const obj4 = { 5:"e" , 6:"f"}
+
+// const obj3 = {obj1 , obj2} // not good method it return like { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+// const obj3 = Object.assign(obj1 , obj2) // not right syntax for more than two object . syntactically the obj2 goes to inside  obj1. so, this is not right
+
+// const obj3 = Object.assign({} , obj1 , obj2) // correct method to merge object for more than two object . obj2 not going to inside of  obj1.
+
+// const obj3 = Object.assign({} , obj1 , obj2 , obj4) // here {}(empty object) it uses to define that we are getting an object and the obj2 , obj4 is not going to inside of obj1.
+// but mostly used spread (...) operator to merge object 
+const obj3 = {...obj1 , ...obj2 , ...obj4} // spread operator to merge object
+
+// console.log(obj3)
+// important method for object
+
+console.log(Object.keys(tinderUser)); // get all the keys of tinderUser
+console.log(Object.values(tinderUser)); // get all the value of tinderUser keys
+console.log(Object.entries(tinderUser));
+
+// to find any values in object 
+console.log(tinderUser.hasOwnProperty('name')); // return true / false value
