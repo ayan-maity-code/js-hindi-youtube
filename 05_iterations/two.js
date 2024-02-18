0a4952e483cd0f3be0b811045cfa267117f93c78
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-18 20:40:26 +0000

    do while and  while

---
+++ 05_iterations/two.js
@@ -0,0 +1,11 @@
+let i =0
+// while (i<=10) {
+//     console.log(i);
+//     i = i+2
+// }
+
+
+do {
+    console.log(i);
+    i = i+1
+} while(i<=10) // do while execute oly once if the condition is not true
