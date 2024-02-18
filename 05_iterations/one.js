557537ef5307a1af21866d382c60a3caecda96fe
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-18 20:24:57 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);  
+// }
+
+// let myArray = ["batman" , "flash" , "superman"]
+// console.log(myArray.length);
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element)
+    
+// }
+
+//break and continue
+
+// for (let i = 1; i < 20; i++) {
+//     if(i === 5){
+//         console.log(`value of i = ${i} detected`)
+//         break
+//     }
+//     console.log(i)
+    
+// }
+
+for (let i = 1; i < 20; i++) {
+    if(i === 5){
+        console.log(`value of i = ${i} detected`)
+        continue
+    }
+    console.log(i)
+    
+}
