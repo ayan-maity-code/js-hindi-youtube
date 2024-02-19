551becbcacb6f5ac1417537bd16721bc4e86398c
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-19 20:30:07 +0000

    for each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,16 @@
+const coding = ["cpp" , "ruby" , "python"]
+
+coding.forEach(function (items) {
+    // console.log(items)
+})
+
+coding.forEach((values) => {
+    // console.log(values);
+    
+})
+
+function printme(items) {
+    console.log(items)
+}
+
+coding.forEach(printme)
