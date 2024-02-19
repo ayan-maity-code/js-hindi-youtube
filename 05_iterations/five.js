2a1815ca5fd99a94b4b375083d7db7a2666c6fdb
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-19 20:47:24 +0000

    #

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -13,4 +13,28 @@ function printme(items) {
     console.log(items)
 }
 
-coding.forEach(printme)
+// coding.forEach(printme)
+
+coding.forEach((item , index , arr) =>{
+    // console.log(item , index , arr);
+})
+
+//+++++++++++++ important(how to get keys and values of object(more than one) which is in the array)
+const myCoding = [
+    {
+        langName : "java",
+        fileName : "java"
+    } ,
+    {
+        langName : "python",
+        fileName : "py"
+    } ,
+    {
+        langName : "c++",
+        fileName : "cpp"
+    }
+]
+
+myCoding.forEach((values) =>{
+    console.log(values.fileName);
+})
