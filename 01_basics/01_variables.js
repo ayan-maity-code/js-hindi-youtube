9f0982fd269034b53b0945aa74e64eb82a435432
Ayan Maity <119981555+ayan-maity-code@users.noreply.github.com>
2024-02-20 15:59:18 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,6 @@ var studentRoll = 156;
 enrollmentNo = 345;
 let className;
 
-/* var is not used bcause of scope problem . Instead of var let is used*/
+/* var is not used because of scope problem . Instead of var let is used*/
 console.log(studentId)
 console.table([studentName,studentRoll,enrollmentNo,className])//for printing multiple values in tabular form
